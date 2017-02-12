bebfc998c97ed9792439907b0c3e21705f03c43d
Madel Asistio <madel.asistio@gmail.com>
2017-02-11 16:06:35 -0800

    low res image loads first

--- MovieViewer/MovieViewer/DetailViewController.swift
+++ MovieViewer/MovieViewer/DetailViewController.swift
@@ -36,13 +36,62 @@ class DetailViewController: UIViewController {
         overviewLabel.sizeToFit()
         
         
-        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let lowResUrl = "https://image.tmdb.org/t/p/w45"
+        let highResUrl = "https://image.tmdb.org/t/p/original"
         
-        if let posterPath = movie["poster_path"] as? String {
-            let imageUrl = NSURL(string: baseUrl + posterPath)
-            posterImageView.setImageWith(imageUrl! as URL)
+        if let posterPath = movie["poster_path"] as? String
+        {
+            
+            let smallImageRequest = NSURLRequest(url: NSURL(string: lowResUrl + posterPath) as! URL)
+            
+            let largeImageRequest = NSURLRequest(url: NSURL(string: highResUrl + posterPath) as! URL)
+            
+            self.posterImageView.setImageWith(
+                smallImageRequest as URLRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallImage;
+                    
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                        
+                        self.posterImageView.alpha = 1.0
+                        
+                    }, completion: { (success) -> Void in
+                        
+                        // The AFNetworking ImageView Category only allows one request to be sent at a time
+                        // per ImageView. This code must be in the completion block.
+                        self.posterImageView.setImageWith(
+                            largeImageRequest as URLRequest,
+                            placeholderImage: smallImage,
+                            success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                
+                                self.posterImageView.image = largeImage;
+                                
+                        },
+                            failure: { (request, response, error) -> Void in
+                                // do something for the failure condition of the large image request
+                                // possibly setting the ImageView's image to a default image
+                        })
+                    })
+            },
+                failure: { (request, response, error) -> Void in
+                    
+                    let largeImageUrl = NSURL(string: highResUrl + posterPath)
+                    
+                    self.posterImageView.setImageWith(largeImageUrl! as URL)
+                    
+                    // do something for the failure condition
+                    // possibly try to get the large image
+                    
+                    
+            /*let imageUrl = NSURL(string: baseUrl + posterPath)
+            posterImageView.setImageWith(imageUrl! as URL) */
+            })
         }
-
         // Do any additional setup after loading the view.
     }
 
